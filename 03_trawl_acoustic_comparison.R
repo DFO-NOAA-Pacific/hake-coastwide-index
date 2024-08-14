d1280650b9fac7f4ae4adbcea226ec7230f35a60
ericward-noaa <eric.ward@noaa.gov>
2024-08-14 06:52:25 -0700

    updated fig, rescaling

--- 03_trawl_acoustic_comparison.R
+++ 03_trawl_acoustic_comparison.R
@@ -11,15 +11,23 @@ wctbs <- readRDS("indices/doy.rds") |>
   dplyr::mutate(survey="trawl")
 
 # scale acoustic to be on same mean in 2003
-acoustic$est <- acoustic$est * as.numeric((wctbs[wctbs$year==2003,"est"] / acoustic[acoustic$year==2003,"est"]))
+cnst <- as.numeric(acoustic[acoustic$year==2003,"est"])
+acoustic$est <- acoustic$est / cnst
+acoustic$lwr <- acoustic$lwr / cnst
+acoustic$upr <- acoustic$upr / cnst
+
+cnst <- as.numeric(wctbs[wctbs$year==2003,"est"])
+wctbs$est <- wctbs$est / cnst
+wctbs$lwr <- wctbs$lwr / cnst
+wctbs$upr <- wctbs$upr / cnst
 
 indices <- rbind(acoustic, wctbs)
 
-ggplot(indices, aes(year, est, group=survey)) + 
+ggplot(dplyr::filter(indices,year>=2003), aes(year, est, group=survey)) + 
   geom_ribbon(aes(ymin=lwr,ymax=upr, fill = survey), alpha=0.3) + 
   geom_line(aes(col = survey),alpha=0.6) +
   geom_point(aes(col=survey),alpha=0.6) + 
-  xlab("Year") + ylab("Estimate") + 
+  xlab("Year") + ylab("Scaled estimate") + 
   scale_color_viridis_d(option="magma", begin=0.3, end=0.8) + 
   scale_fill_viridis_d(option="magma", begin=0.3, end=0.8) + 
   theme_bw()
